7bc0e0811ac4af825e17606646edccbfac1a511c
3XP1017 <3XP1017@localhost.localdomain>
2019-06-08 20:12:04 +0100

    Removed old terraform files that had no purpose

--- provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region                  = "${var.provider["region"]}"
-  shared_credentials_file = "${var.provider["shared_creds_file"]}"
-  profile                 = "${var.provider["aws_profile"]}"
-}
